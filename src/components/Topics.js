d6edc21757b831563f1e7feafd955a29d8988049
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-02-17 11:27:29 +0530

    Update Topics.js

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -3,22 +3,25 @@ import { Link, useHistory } from "react-router-dom";
 
 const Topics = () => {
   const history = useHistory();
-  const backHandle = () => {
-    history.goBack();
-  }
   return (
     <div id="topics-div">
       <h2>Topics</h2>
       <ul>
         <li>
-          <h2 id="rendering"><Link to={"/topics/rendering"}>Rendering</Link></h2>
+          <Link to="/topics/rendering">
+            <h2 id="rendering">Rendering</h2>
+          </Link>
         </li>
         <li>
-          <h2 id="components"><Link to={"/topics/components"}>Components</Link></h2>
+          <Link to="/topics/components">
+            <h2 id="components">Components</h2>
+          </Link>
         </li>
       </ul>
       <hr />
-      <button id="topics-back-button" onClick={backHandle}>Back</button>
+      <Link to="/">
+        <button onClick={history.goBack} id="topics-back-button">Back</button>
+      </Link>
     </div>
   );
 };
