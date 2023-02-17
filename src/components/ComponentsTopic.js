b59344a1a39bda5571aa9e94b7456925d57d0f17
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-02-17 11:28:44 +0530

    Update ComponentsTopic.js

--- src/components/ComponentsTopic.js
+++ src/components/ComponentsTopic.js
@@ -3,13 +3,11 @@ import { useHistory } from "react-router-dom";
 
 const ComponentsTopic = () => {
   const history = useHistory();
-  const backHandle = () => {
-    history.goBack();
-  }
+
   return (
     <div id="components-div">
       <h3>Components in React</h3>
-      <button id="components-back-button" onClick={backHandle}>
+      <button onClick={history.goBack} id="components-back-button">
         Back
       </button>
     </div>
